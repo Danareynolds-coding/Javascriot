fba13d21de701ab2746f039a8606ea4b0783f4c3
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-06-11 09:34:24 -0500

    JS

---
+++ JS/app.js
@@ -0,0 +1,48 @@
+// legacy variable keyword
+var age = 24;
+// blocked scoped variable
+let name = "michelle";
+//const keeps variable the same it doesn't need to be changed
+const isStudent = false;
+// primitive data types
+let tempature=98.6;   //*number       
+let greeting="hello"; //*string
+let IsLoggedin=false; //*Boolean
+let emptyvalue = null; //*null
+let notDefined; //*undefined
+// basic setup for sending data to html- consol.log
+console.log("hey  trey")
+console.log(name);
+console.log(IsLoggedin);
+// arthmetic Operators
+let one = 10;
+let two = 3;
+let sum =one+two;
+console.log(sum); //*13
+let difference = one-two;
+console.log(difference);//*7
+let product = one*two;
+console.log(product);
+let quotient = one/two;
+console.log(quotient); //3.3333335
+let remainder = one % two;
+console.log(remainder) //*1
+
+// increment and decrement
+one++ //*11 
+// same as  one = one + 1
+console.log(one);
+one-- 
+console.log(one) //*10
+
+// assignment Opperators
+let numthree = 5;
+numthree = numthree +2;
+// same as numthree +=2;
+console.log(numthree); //*7
+numthree*= 3;
+console.log(numthree); //*21
+
+
+
+
